fda02b9f4f976c27cc0bd6a5d450cc23f5446f15
rohinipm <mrohini216@gmail.com>
2023-08-08 14:06:46 +0530

    using objects in class component

--- src/components1/Car1.js
+++ src/components1/Car1.js
@@ -3,15 +3,19 @@ import React from "react";
 class Car1 extends React.Component {
   constructor() {
     super();
-    this.state = { color: "Red" };
+    this.state = { color: "Red", model: "Ford" };
   }
   render() {
     return (
       <>
-        <h1>My car color is {this.state.color}</h1>
+        <h1>
+          My car color is {this.state.color} and the model is {this.state.model}
+        </h1>
         <button
           onClick={() => {
-            this.setState({ color: "Blue" });
+            this.setState((previousState) => {
+              return { ...previousState, color: "Blue" };
+            });
           }}
         >
           Change Color
