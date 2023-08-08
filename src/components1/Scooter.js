dc70390eb4c42886e59ebf955cf1804fce1232f8
rohinipm <mrohini216@gmail.com>
2023-08-08 13:21:12 +0530

    state with object

--- src/components1/Scooter.js
+++ src/components1/Scooter.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
 
 function Scooter() {
-  const [color, setColor] = useState("Red");
-  const [brand, setbrand] = useState("Honda");
-  const [model, setmodel] = useState("Activa 6G");
-  const [year, setyear] = useState("2020");
+  //   const [color, setColor] = useState("Red");
+  //   const [brand, setbrand] = useState("Honda");
+  //   const [model, setmodel] = useState("Activa 6G");
+  //   const [year, setyear] = useState("2020");
+
+  const [scooter, setScooter] = useState({
+    color: "Red",
+    brand: "Honda",
+    model: "Activa 6G",
+    year: "2020",
+  });
 
   return (
     <>
       <h1>My Scooter</h1>
-      <p>Color: {color}</p>
-      <p>Brand: {brand}</p>
-      <p>Model: {model}</p>
-      <p>Year: {year}</p>
+      <p>Color: {scooter.color}</p>
+      <p>Brand: {scooter.brand}</p>
+      <p>Model: {scooter.model}</p>
+      <p>Year: {scooter.year}</p>
     </>
   );
 }
