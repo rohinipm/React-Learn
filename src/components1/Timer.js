4fe4cb31333c28ade456a31ddcb0f72e11dbd5ef
rohinipm <mrohini216@gmail.com>
2023-08-09 11:25:13 +0530

    use effect diff ways

--- src/components1/Timer.js
+++ src/components1/Timer.js
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 
 function Timer() {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
 
-  useEffect(
-    (previousState) => {
-      console.log("Screen Rendered");
-      checkCount();
-    },
-    [count]
-  );
+  useEffect((previousState) => {
+    console.log("Screen Rendered");
+    //setCount(1);
+    //checkCount();
+    // setTimeout(() => {
+    //   setCount((previousState) => {
+    //     return previousState + 1;
+    //   });
+    // }, 1000);
+  });
 
   function updateCount() {
     setCount((previousState) => {
