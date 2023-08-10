0875530368d253e0a81e51b81fcb8ceae1178106
rohinipm <mrohini216@gmail.com>
2023-08-10 11:19:24 +0530

    Form value simlify

--- src/components2/MyForm.js
+++ src/components2/MyForm.js
@@ -6,52 +6,54 @@ function MyForm() {
   //   const [email, setEmail] = useState("");
   // console.log("current state", name);
 
-  const [inputs, setInputs] = useState("");
+  const [inputs, setInputs] = useState({ phone: "+91", email: "@gmail.com" });
 
   function handleSubmit(e) {
     e.preventDefault();
     console.log("Form Submitted");
     console.log("Current state", inputs);
   }
+
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((previousState) => {
+      return { ...previousState, [name]: value };
+    });
+  }
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        Enter Your Name:{" "}
+        Enter Your Name:
+        <input type="text" name="name" onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Enter Your Age:
+        <input type="text" name="age" onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Enter Your Email:
         <input
           type="text"
-          onChange={(e) => {
-            setInputs((previousState) => {
-              return { ...previousState, name: e.target.value };
-            });
-          }}
+          name="email"
+          onChange={handleChange}
+          value={inputs.email}
         />
       </label>
       <br />
       <label>
-        Enter Your Age:{" "}
+        Enter Your Phone:
         <input
           type="text"
-          onChange={(e) => {
-            setInputs((previousState) => {
-              return { ...previousState, age: e.target.value };
-            });
-          }}
+          name="phone"
+          onChange={handleChange}
+          value={inputs.phone}
         />
       </label>
       <br />
-      <label>
-        Enter Your Email:{" "}
-        <input
-          type="text"
-          onChange={(e) => {
-            setInputs((previousState) => {
-              return { ...previousState, email: e.target.value };
-            });
-          }}
-        />
-      </label>
-      <br />
-      <input type="submit" value="Submit From" />;
+      <input type="submit" value="Submit From" />
     </form>
   );
 }
